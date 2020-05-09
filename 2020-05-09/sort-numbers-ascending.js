1621c97c5df47a05ce07d4fe429369c1175da1d0
Coen van Steijn <kewner@gmail.com>
2020-05-09 19:34:29 +0200

    9 May challenges

---
+++ 2020-05-09/sort-numbers-ascending.js
@@ -0,0 +1,22 @@
+// Create a function that takes an array of numbers and returns a new array,
+// sorted in ascending order (smallest to biggest).
+
+// - Sort numbers array in ascending order.
+// - If the function's argument is null, an empty array, or undefined;
+//   return an empty array.
+// - Return a new array of sorted numbers.
+// - Test input can be positive or negative.
+
+function sortNumsAscending(arr) {
+  return arr !== null ? arr.sort((a, b) => a - b) : [];
+}
+
+console.log(sortNumsAscending([1, 2, 10, 50, 5])) // [1, 2, 5, 10, 50]
+console.log(sortNumsAscending([80, 29, 4, -95, -24, 85])) // [-95, -24, 4, 29, 80, 85]
+console.log(sortNumsAscending(null)) // []
+console.log(sortNumsAscending([])) // []
+
+// Alternative solution
+function sortNumsAscending(arr) {
+  return (arr || []).sort((a,b) => a - b)
+}
